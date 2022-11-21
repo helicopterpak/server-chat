4eee20327a09ba4b6b93a29964638880b37b5879
Pak <helicopterpak@gmail.com>
2022-11-21 17:38:48 +0300

    new lesson

---
+++ src/main/java/ru/itsjava/services/Observable.java
@@ -0,0 +1,7 @@
+package ru.itsjava.services;
+
+public interface Observable {
+    void addObserver(Observer observer);
+    void deleteObserver(Observer observer);
+    void notifyObserver(String message);
+}
